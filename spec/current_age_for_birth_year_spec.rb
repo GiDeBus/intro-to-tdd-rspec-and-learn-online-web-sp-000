6cd8a1d560afc09df0a495375ae032280009860b
immutable-integer-8885 <gidelbusto@gmail.com>
2020-03-14 00:07:18 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,7 +1,9 @@
 require_relative '../current_age_for_birth_year.rb'
 
 def
-current_age_for_birth_year(birth_year)
+current_age_for_birth_year(
+  birth_year)
+    2003 - birth_year
 end
 
 describe "current_age_for_birth_year method" do
